0360c41e6ca3c0420ee425b9c80e862984e03f18
Tue Bui <tuebui2806@gmail.com>
2022-09-19 11:46:08 -0400

    initial commit

---
+++ SF_petclinic/src/test/java/huytue/SF_petclinic/SfPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package huytue.SF_petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
