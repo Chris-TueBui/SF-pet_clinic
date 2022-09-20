d40e2da42c143cc5f21dac7e8d4bb28b9217edb4
Tue Bui <tuebui2806@gmail.com>
2022-09-20 17:00:07 -0400

    creating pojo

---
+++ SF_petclinic/src/main/java/huytue/SF_petclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package huytue.SF_petclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
